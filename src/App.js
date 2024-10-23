787aca39c580a379067bb58a8c1d72c92b4aee97
Sujin Lim <920907vv@gmail.com>
2024-10-23 10:20:02 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 페이지 로드 시 /cw2024로 리다이렉트
+    navigate("/cw2024");
+  }, [navigate]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redirecting to /cw2024...</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import poster from "./images/poster.jpg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<App />} />
+      <Route
+        path="/cw2024"
+        element={
+          <div>
+            <img
+              src={poster}
+              alt="Poster"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </div>
+        }
+      />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
